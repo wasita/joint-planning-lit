dce32389f75272d0bf389aa7f306067b3fbac374
wasita <wasita.mahaphanit@gmail.com>
2026-09-24 17:28:57 -0400

    🚧 init stag hunt simulation marimo notebook

---
+++ notebooks/simulate_staghunt.py
@@ -0,0 +1,24 @@
+import marimo
+
+__generated_with = "0.24.2"
+app = marimo.App()
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Simulate a 2-player stag hunt game
+
+
+    - Payoff matrix:
+    """)
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
